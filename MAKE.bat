d8a3e60d981cfc94e3f6d264932023c8af2cae85
Prathyush Poduval <prathyushpoduval@gmail.com>
2020-09-25 09:03:18 +0530

    Basic Site

---
+++ MAKE.bat
@@ -0,0 +1,6 @@
+python .\jemdoc -c .\mysite.conf .\index.jemdoc
+python .\jemdoc -c .\mysite.conf .\resume.jemdoc
+python .\jemdoc -c .\mysite.conf .\research.jemdoc
+python .\jemdoc -c .\mysite.conf .\group.jemdoc
+python .\jemdoc -c .\mysite.conf .\contact.jemdoc
+python .\jemdoc -c .\mysite.conf .\mathjax.jemdoc
